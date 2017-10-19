4cc6c59cb64edb879d9ad6807da3c2e90717f76a
axlwild <axel_cg4@hotmail.com>
2017-10-19 14:08:26 -0500

    Add files via upload

---
+++ main_prac9.cpp
@@ -0,0 +1,323 @@
+//Semestre 2018 - 1
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//*************											******//
+//*************											******//
+//************************************************************//
+//************************************************************//
+
+#include "texture.h"
+#include "figuras.h"
+#include "Camera.h"
+
+
+//NEW//////////////////NEW//////////////////NEW//////////////////NEW////////////////
+static GLuint ciudad_display_list;	//Display List for the Monito
+
+
+int w = 500, h = 500;
+int frame=0,time,timebase=0;
+int deltaTime = 0;
+char s[30];
+
+CCamera objCamera;	//Create objet Camera
+
+GLfloat g_lookupdown = 0.0f;    // Look Position In The Z-Axis (NEW) 
+
+int font=(int)GLUT_BITMAP_HELVETICA_18;
+
+
+//GLfloat Diffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };				// Diffuse Light Values
+GLfloat Diffuse[]= { 0.5f, 0.5f, 0.5f, 1.0f };				// Diffuse Light Values
+GLfloat Specular[] = { 1.0, 1.0, 1.0, 1.0 };				// Specular Light Values
+GLfloat Position[]= { 0.0f, 7.0f, -5.0f, 0.0f };			// Light Position
+GLfloat Position2[]= { 0.0f, 0.0f, -5.0f, 1.0f };			// Light Position
+
+CTexture text1;
+CTexture text2;	//Pavimento
+CTexture text3;	//Casa1
+CTexture t_cristal; //se crea textura de cristal
+CFiguras cubo;
+CFiguras sky;
+
+//END NEW//////////////////////////////////////////
+
+
+CFiguras fig1;//pavimento
+CFiguras fig2;//skybox
+CFiguras fig3;	//Casa
+
+
+void ciudad ()
+{
+
+	glEnable(GL_ALPHA_TEST); //hanilita el canal alpha (para transparencia)****
+	glAlphaFunc(GL_GREATER, 0.1); //qué tipo de transparencia, esos parámetros son los comunes***
+
+	glPushMatrix(); //Camino1
+			//glTranslatef(23.5,0.0,0.0);
+			glScalef(100,0.1,7);
+			glDisable(GL_LIGHTING);
+			fig2.prisma2(text2.GLindex, 0);
+			glEnable(GL_LIGHTING);
+		glPopMatrix();		
+
+		
+		glPushMatrix(); //Casa1 a esta casa se le aplicará la nueva textura 
+			glTranslatef(0.0,3.0,-6.0);
+			glRotatef(90,1,0,0);
+			//glRotatef(180,0,0,1);
+			glScalef(6,5.0,6);
+			glDisable(GL_LIGHTING);
+			//glEnable(GL_BLEND);
+			//glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+			fig3.prisma2(t_cristal.GLindex, text1.GLindex);
+			//glDisable(GL_BLEND);
+			glEnable(GL_LIGHTING);
+		glPopMatrix();
+
+		glPushMatrix(); //Casa2
+			glTranslatef(0.0,3.0,6);
+			glRotatef(90,1,0,0);
+			glRotatef(180,0,0,1);
+			glScalef(6,5.0,6);
+			glDisable(GL_LIGHTING);
+			fig3.prisma2(text3.GLindex, 0);
+			glEnable(GL_LIGHTING);
+		glPopMatrix();
+		//desactive
+		glDisable(GL_ALPHA_TEST); 
+}
+
+GLuint createDL() 
+{
+	GLuint ciudadDL;
+	//GLuint cieloDL;
+
+	// Create the id for the list
+	ciudadDL = glGenLists(1);
+	// start list
+	glNewList(ciudadDL,GL_COMPILE);
+	// call the function that contains 
+	// the rendering commands
+		ciudad();
+	// endList
+	glEndList();
+
+	return(ciudadDL);
+}
+			
+void InitGL ( GLvoid )     // Inicializamos parametros
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo	
+
+	glEnable(GL_TEXTURE_2D);  //indica a opengl que utilice texturizado 2d****
+
+	glShadeModel (GL_SMOOTH);
+	//Para construir la figura comentar esto
+	glLightfv(GL_LIGHT1, GL_POSITION, Position);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, Diffuse);
+	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, Position2);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	glEnable ( GL_COLOR_MATERIAL );
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	//glPolygonMode(GL_BACK, GL_LINE);
+
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+    
+    text1.LoadBMP("01.bmp");
+	text1.BuildGLTexture();
+	text1.ReleaseImage();
+
+
+	text2.LoadTGA("pavimento.tga");
+	text2.BuildGLTexture();
+	text2.ReleaseImage();
+
+	text3.LoadTGA("casa01.tga");
+	text3.BuildGLTexture();
+	text3.ReleaseImage();
+	//Crear textura nueva que se pueda utilizar en base a casa_base.tga
+	
+
+
+	t_cristal.LoadTGA("cristal.tga");	//abre la imagen, comprueba formato, y la carga en un arreglo de datos.
+	t_cristal.BuildGLTexture();			//Lo convierte a una textura como arreglo de datos
+	t_cristal.ReleaseImage();			//libera el archivo de la imagen (libera memoria)
+
+
+	//END NEW//////////////////////////////
+
+	objCamera.Position_Camera(0,2.5f,3, 0,2.5f,0, 0, 1, 0);
+
+	//NEW Crear una lista de dibujo
+	ciudad_display_list = createDL();
+
+}
+
+void pintaTexto(float x, float y, float z, void *font,char *string)
+{
+  
+  char *c;     //Almacena los caracteres a escribir
+  glRasterPos3f(x, y, z);	//Posicion apartir del centro de la ventana
+  //glWindowPos2i(150,100);
+  for (c=string; *c != '\0'; c++) //Condicion de fin de cadena
+  {
+    glutBitmapCharacter(font, *c); //imprime
+  }
+}
+
+void display ( void )   // Creamos la funcion donde se dibuja
+{
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glLoadIdentity();
+	
+		
+	glPushMatrix();
+		glRotatef(g_lookupdown,1.0f,0,0);
+
+		gluLookAt(	objCamera.mPos.x,  objCamera.mPos.y,  objCamera.mPos.z,	
+					objCamera.mView.x, objCamera.mView.y, objCamera.mView.z,	
+					objCamera.mUp.x,   objCamera.mUp.y,   objCamera.mUp.z);
+	
+
+		glPushMatrix();		
+			glPushMatrix(); //Creamos cielo
+				glDisable(GL_LIGHTING);
+				glTranslatef(0,60,0);
+				fig1.skybox(130.0, 130.0, 130.0,text1.GLindex);
+				glEnable(GL_LIGHTING);
+			glPopMatrix();
+
+			glPushMatrix();
+				glEnable ( GL_COLOR_MATERIAL );
+					glColor3f(1, 1, 1);
+					glCallList(ciudad_display_list);
+				glDisable ( GL_COLOR_MATERIAL );
+			glPopMatrix();
+			
+		
+	glPopMatrix();
+
+	glutSwapBuffers ( );
+
+}
+
+
+
+void reshape ( int width , int height )   // Creamos funcion Reshape
+{
+  if (height==0)										// Prevenir division entre cero
+	{
+		height=1;
+	}
+
+	glViewport(0,0,width,height);	
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+	
+	glFrustum (-0.1, 0.1,-0.1, 0.1, 0.1, 170.0);
+
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();
+}
+
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+	switch ( key ) {
+
+		case 'w':   //Movimientos de camara
+		case 'W':
+			objCamera.Move_Camera( CAMERASPEED+0.2 );
+			break;
+
+		case 's':
+		case 'S':
+			objCamera.Move_Camera(-(CAMERASPEED+0.2));
+			break;
+
+		case 'a':
+		case 'A':
+			objCamera.Strafe_Camera(-(CAMERASPEED+0.4));
+			break;
+
+		case 'd':
+		case 'D':
+			objCamera.Strafe_Camera( CAMERASPEED+0.4 );
+			break;
+
+		case 27:        // Cuando Esc es presionado...
+			exit ( 0 );   // Salimos del programa
+			break;        
+		default:        // Cualquier otra
+			break;
+  }
+
+  glutPostRedisplay();
+}
+
+void arrow_keys ( int a_keys, int x, int y )  // Funcion para manejo de teclas especiales (arrow keys)
+{
+  switch ( a_keys ) {
+	case GLUT_KEY_PAGE_UP:
+		objCamera.UpDown_Camera(CAMERASPEED);
+		break;
+
+	case GLUT_KEY_PAGE_DOWN:
+		objCamera.UpDown_Camera(-CAMERASPEED);
+		break;
+
+    case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		g_lookupdown -= 1.0f;
+		break;
+
+    case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		g_lookupdown += 1.0f;
+		break;
+
+	case GLUT_KEY_LEFT:
+		objCamera.Rotate_View(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_RIGHT:
+		objCamera.Rotate_View( CAMERASPEED);
+		break;
+
+    default:
+		break;
+  }
+  glutPostRedisplay();
+}
+
+int main ( int argc, char** argv )   // Main Function
+{
+
+  glutInit            (&argc, argv); // Inicializamos OpenGL
+  glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+  glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
+  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutCreateWindow    ("Práctica 9"); // Nombre de la Ventana
+  //glutFullScreen     ( );         // Full Screen
+  InitGL ();						// Parametros iniciales de la aplicacion
+  glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
+  glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
+  glutKeyboardFunc    ( keyboard );	//Indicamos a Glut función de manejo de teclado
+  glutSpecialFunc     ( arrow_keys );	//Otras
+  glutMainLoop        ( );          // 
+
+  return 0;
+}
